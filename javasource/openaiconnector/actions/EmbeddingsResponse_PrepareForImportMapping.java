5ccfc7a54ca381edbd513d20d9684733f2dac9d0
liamsommer-mx <liam.sommer@mendix.com>
2025-05-07 07:45:11 +0000

    SAS-1252: removed duplicated GenAIImpl java files and reordered imports

--- javasource/openaiconnector/actions/EmbeddingsResponse_PrepareForImportMapping.java
+++ javasource/openaiconnector/actions/EmbeddingsResponse_PrepareForImportMapping.java
@@ -15,8 +15,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mendix.systemwideinterfaces.core.IContext;
-import com.mendix.webui.CustomJavaAction;
 import com.mendix.systemwideinterfaces.core.UserAction;
+import com.mendix.webui.CustomJavaAction;
 
 public class EmbeddingsResponse_PrepareForImportMapping extends UserAction<java.lang.String>
 {
