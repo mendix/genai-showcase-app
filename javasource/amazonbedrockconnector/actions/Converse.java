ef41643ed31fa8aa47b26fd9d4d1f0ce0e9b15f3
liamsommer-mx <liam.sommer@mendix.com>
2025-06-10 07:17:58 +0000

    SAS-1317: changed Converse.java to not hardcode the document names anymore, but use the FileContent.FileName attribute instead

--- javasource/amazonbedrockconnector/actions/Converse.java
+++ javasource/amazonbedrockconnector/actions/Converse.java
@@ -359,10 +359,6 @@ public class Converse extends UserAction<IMendixObject>
 				contentBlockList.add(textContent);
 			}
 			
-			// Count for documents in message
-			// Used for unique document name
-			int j = 0; 
-			
 			// Adding file content for each file
 			List<FileContent> files = getFiles(mxMsg);
 			for (FileContent file : files) {
@@ -392,9 +388,8 @@ public class Converse extends UserAction<IMendixObject>
 						break;
 					}
 					case document: {
-						ContentBlock documentContentBlock = getDocumentContent(file, i, j);
+						ContentBlock documentContentBlock = getDocumentContent(file);
 						contentBlockList.add(documentContentBlock);
-						j++;
 						break;
 					}
 					default:
@@ -467,17 +462,16 @@ public class Converse extends UserAction<IMendixObject>
 		return true;
 	}
 	
-	private ContentBlock getDocumentContent(FileContent doc, int i, int j) {
+	private ContentBlock getDocumentContent(FileContent doc) {
 		// Creating document content block
 		// Using fixed name because this field is vulnerable to prompt injection
 		// source is fileContent attribute as byte[] from base64 string
 		String format = getFileExtension(doc);
-		String name = String.format("%s-%s-%s", DOC_NAME, i, j);
 		DocumentSource source = getDocSource(doc);
 		
 		DocumentBlock docBlock = DocumentBlock.builder()
 				.format(format)
-				.name(name)
+				.name(doc.getFileName())
 				.source(source)
 				.build();
 		
