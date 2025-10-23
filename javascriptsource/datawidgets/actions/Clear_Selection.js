caf02110ea1fb1188f545aad23d6c54f002d5591
Yinnadie Catalan <yinnadie.catalan@mendix.com>
2025-10-23 18:50:34 +0000

    [SAS-1078] React Client compatibility - updated data Widget to v 3.6.1

--- javascriptsource/datawidgets/actions/Clear_Selection.js
+++ javascriptsource/datawidgets/actions/Clear_Selection.js
@@ -5,22 +5,21 @@
 // - the code between BEGIN USER CODE and END USER CODE
 // - the code between BEGIN EXTRA CODE and END EXTRA CODE
 // Other code you write will be lost the next time you deploy the project.
-import { Big } from "big.js";
 import "mx-global";
+import { Big } from "big.js";
 
 // BEGIN EXTRA CODE
 // END EXTRA CODE
 
 /**
- * @param {string} targetName - Name of the widget for which filters should be reset. Valid targets are: Data grid 2, Gallery. You can find filter name in widget settings in the "Common" group (Properties>Common>Name).
- * @param {boolean} setToDefault - Set to default value
+ * @param {string} targetName - The name of the widget for which selection should be cleared.
  * @returns {Promise.<void>}
  */
-export async function Reset_All_Filters(targetName, setToDefault) {
+export async function Clear_Selection(targetName) {
 	// BEGIN USER CODE
     const plugin = window["com.mendix.widgets.web.plugin.externalEvents"];
     if (plugin) {
-        plugin.emit(targetName, "reset.filters", setToDefault);
+        plugin.emit(targetName, "selection.clear");
     }
 	// END USER CODE
 }
