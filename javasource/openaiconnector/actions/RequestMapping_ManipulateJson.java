fa1b814ff71df6935dec4985a1bac870d2d8c502
Lina <lina.fischer@mendix.com>
2026-01-09 15:54:26 +0000

    SAS-1652 - changed RequestMapping manipulation for newer models and removed unused parameters for o models

--- javasource/openaiconnector/actions/RequestMapping_ManipulateJson.java
+++ javasource/openaiconnector/actions/RequestMapping_ManipulateJson.java
@@ -46,12 +46,14 @@ public class RequestMapping_ManipulateJson extends UserAction<java.lang.String>
 	private final openaiconnector.proxies.RequestMapping RequestMapping;
 	private final java.lang.String Request_Json;
 	private final java.lang.String Architecture;
+	private final java.lang.Boolean IsLegacyModel;
 
 	public RequestMapping_ManipulateJson(
 		IContext context,
 		IMendixObject _requestMapping,
 		java.lang.String _request_Json,
-		java.lang.String _architecture
+		java.lang.String _architecture,
+		java.lang.Boolean _isLegacyModel
 	)
 	{
 		super(context);
@@ -59,6 +61,7 @@ public class RequestMapping_ManipulateJson extends UserAction<java.lang.String>
 		this.RequestMapping = _requestMapping == null ? null : openaiconnector.proxies.RequestMapping.initialize(getContext(), _requestMapping);
 		this.Request_Json = _request_Json;
 		this.Architecture = _architecture;
+		this.IsLegacyModel = _isLegacyModel;
 	}
 
 	@java.lang.Override
@@ -79,6 +82,7 @@ public class RequestMapping_ManipulateJson extends UserAction<java.lang.String>
 			updateMessages(rootNode);
 			setFunctionToolChoice(rootNode);
 			mapFunctionParameters();
+			handleMaxTokensParameter(rootNode);
 
 			return MAPPER.writeValueAsString(rootNode);
 		} catch (Exception e) {
@@ -417,6 +421,29 @@ public class RequestMapping_ManipulateJson extends UserAction<java.lang.String>
 		parametersNode.set("required", requiredNode);
 		
 		return parametersNode;
+	}
+	
+	private void handleMaxTokensParameter(JsonNode rootNode) {
+		// If IsLegacyModel is not set (null), follow the legacy flow (do nothing)
+		if (IsLegacyModel == null) {
+			LOGGER.debug("IsLegacyModel parameter is not set. Following legacy flow.");
+			return;
+		}
+		
+		// If IsLegacyModel is true, keep max_tokens as is
+		if (IsLegacyModel) {
+			LOGGER.debug("IsLegacyModel is true. Keeping max_tokens parameter.");
+			return;
+		}
+		
+		// If IsLegacyModel is false, rename max_tokens to max_completion_tokens
+		JsonNode maxTokensNode = rootNode.path("max_tokens");
+		if (maxTokensNode != null && !maxTokensNode.isMissingNode()) {
+			int maxTokensValue = maxTokensNode.asInt();
+			((ObjectNode) rootNode).remove("max_tokens");
+			((ObjectNode) rootNode).put("max_completion_tokens", maxTokensValue);
+			LOGGER.debug("IsLegacyModel is false. Renamed max_tokens to max_completion_tokens.");
+		}
 	}		
 	// END EXTRA CODE
 }
