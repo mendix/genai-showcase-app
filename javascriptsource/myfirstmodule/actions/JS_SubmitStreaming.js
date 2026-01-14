54c82cea5efdb5e49bc68ee22f7f689c63b229a0
Ayca Öner <ayca.oner@mendix.com>
2026-01-14 08:17:30 +0000

    SAS-1678: bug fix

--- javascriptsource/myfirstmodule/actions/JS_SubmitStreaming.js
+++ javascriptsource/myfirstmodule/actions/JS_SubmitStreaming.js
@@ -13,7 +13,7 @@ import { Big } from "big.js";
 
 /**
  * @param {string} requestJSON
- * @param {string} deployedModelUUID
+ * @param {string} deployedModel
  * @param {MxObject} responseCollector
  * @returns {Promise.<void>}
  */
